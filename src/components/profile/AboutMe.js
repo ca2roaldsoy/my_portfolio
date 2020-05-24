feda48ac1f50072103222fbe2e5bbd191439f306
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-24 20:39:14 +0200

    added animation to page and importet image

--- src/components/profile/AboutMe.js
+++ src/components/profile/AboutMe.js
@@ -2,18 +2,18 @@ import React from "react";
 
 function AboutMe() {
   return (
-    <section>
-      <h3>About Me</h3>
-      <p>I am a front end developer.</p>
-      <p>
+    <section className="aboutMe">
+      <h3 className="aboutMe__title">About Me</h3>
+      <p className="aboutMe__text">I am a front end developer.</p>
+      <p className="aboutMe__text">
         I enjoy coding and I like to keep things simple and in structure. I also
         like to keep myself updated with the newest things.
       </p>
-      <p>
+      <p className="aboutMe__text">
         I love working in teams as well as doing things on my own. Getting
         others opinion will only help improve projects.
       </p>
-      <p>
+      <p className="aboutMe__quoute">
         <em>
           &laquo; It's very important to have a feedback loop, where you
           constantly thinking about what you've done and how you could be doing
@@ -21,7 +21,7 @@ function AboutMe() {
         </em>
         &raquo;
       </p>
-      <p>
+      <p className="aboutMe__quoute--name">
         <span>&#45; Elon Musk</span>
       </p>
     </section>
--- src/components/profile/MyInfo.js
+++ src/components/profile/MyInfo.js
@@ -1,27 +1,37 @@
 import React from "react";
 import { Col, Row, ListGroupItem, ListGroup, Image } from "react-bootstrap";
+import MyProfileImg from "../../assets/myprofile_image.jpg";
 
 function MyInfo() {
   return (
     <Row id="profile">
-      <Col sm={12} lg={6}>
-        <Image src="../me.jpg" alt="profile of me" fluid rounded />
+      <Col sm={6}>
+        <Image
+          src={MyProfileImg}
+          alt="profile of me"
+          fluid
+          roundedCircle
+          className="animate__animated animate__fadeInDown animate__slow"
+        />
       </Col>
-      <Col sm={12} lg={6} id="profile_info">
+      <Col sm={6} id="profile_info">
         <ListGroup variant="flush">
-          <ListGroupItem>
+          <ListGroupItem className="animate__animated animate__lightSpeedInLeft animate__slow">
             <b>Name:</b> Cato Roaldsøy
           </ListGroupItem>
-          <ListGroupItem>
+          <ListGroupItem className="animate__animated animate__lightSpeedInLeft animate__slower">
             <b>Age:</b> 32 years
           </ListGroupItem>
-          <ListGroupItem>
+          <ListGroupItem className="animate__animated animate__lightSpeedInLeft animate__slow animate__delay-1s">
             <b>Adress:</b> P. Krohns Veg 12
           </ListGroupItem>
-          <ListGroupItem>
-            <b>City:</b> 4352 Kleppe
+          <ListGroupItem className="animate__animated animate__lightSpeedInLeft animate__slower animate__delay-1s">
+            <b>City:</b> 4351 Kleppe
           </ListGroupItem>
-          <a href="https://www.facebook.com/people/Cato-Roalds%C3%B8y/100000434225604">
+          <a
+            href="https://www.facebook.com/people/Cato-Roalds%C3%B8y/100000434225604"
+            className="animate__animated animate__lightSpeedInLeft animate__slower animate__delay-1s"
+          >
             Look me up on Facebook
           </a>
         </ListGroup>
