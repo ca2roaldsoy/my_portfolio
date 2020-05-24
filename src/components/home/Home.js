47b387e17a51a7fa1436a6eb912186ca0ad54fd9
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-24 17:30:17 +0200

    change classname on button click

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Experience from "../experience/Experience";
 import Projects from "../projects/Projects";
@@ -9,6 +9,15 @@ import Row from "react-bootstrap/Row";
 import ParticlesBg from "particles-bg";
 
 function Home() {
+  const [animationClass, setAnimationClass] = useState(true);
+
+  function animateClass() {
+    if (animationClass) {
+      return "animate__bounceInDown";
+    } else {
+      return "animate__bounceOutRight";
+    }
+  }
   return (
     <>
       <ParticlesBg type="cobweb" bg={true} color="#c9c9c9" />
@@ -22,25 +31,49 @@ function Home() {
         </Col>
 
         <Col sm={6} className="home__link">
-          <NavLink to="/profile" role="link">
-            <div className="animate__animated animate__bounceInDown animate__slow home__link--link">
+          <NavLink
+            to="/profile"
+            role="link"
+            onClick={() => setAnimationClass(false)}
+          >
+            <div
+              className={`animate__animated ${animateClass()} animate__slow home__link--link`}
+            >
               Profile
             </div>
           </NavLink>
 
-          <NavLink to="/experience" role="link">
-            <div className="animate__animated animate__bounceInDown animate__slower home__link--link">
+          <NavLink
+            to="/experience"
+            role="link"
+            onClick={() => setAnimationClass(false)}
+          >
+            <div
+              className={`animate__animated ${animateClass()} animate__slower home__link--link`}
+            >
               Experience
             </div>
           </NavLink>
-          <NavLink to="/projects" role="link">
-            <div className="animate__animated animate__bounceInDown animate__slow animate__delay-1s home__link--link">
+          <NavLink
+            to="/projects"
+            role="link"
+            onClick={() => setAnimationClass(false)}
+          >
+            <div
+              className={`animate__animated ${animateClass()} animate__slow animate__delay-1s home__link--link`}
+            >
               Projects
             </div>
           </NavLink>
 
-          <NavLink to="/contact" role="link">
-            <div className="animate__animated animate__bounceInDown animate__slower animate__delay-1s home__link--link">
+          <NavLink
+            to="/contact"
+            role="link"
+            onClick={() => setAnimationClass(false)}
+          >
+            <div
+              className={`animate__animated ${animateClass()} animate__slower animate__delay-1s home__link--link`}
+            >
               Contact
             </div>
           </NavLink>
