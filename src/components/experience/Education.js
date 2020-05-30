f260fa82fba187e67cc854e4fd49c6e88401dafc
ca2roaldsoy <roaldsoyen@gmail.com>
2020-05-30 22:43:56 +0200

    added animation to education

--- src/components/experience/Education.js
+++ src/components/experience/Education.js
@@ -33,104 +33,99 @@ function Education() {
             <Tab.Content>
               <Tab.Pane eventKey="#link1">
                 <ListGroup>
-                  <ListGroup.Item>Autodesk 3ds Max</ListGroup.Item>
-                  <ListGroup.Item>Adobe Photoshop</ListGroup.Item>
-                  <ListGroup.Item>Adobe Premier Pro</ListGroup.Item>
-                  <ListGroup.Item>Design</ListGroup.Item>
-                  <ListGroup.Item>Animation</ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Autodesk 3ds Max
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Adobe Photoshop
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Adobe Premier Pro
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Design
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Animation
+                  </ListGroup.Item>
                 </ListGroup>
               </Tab.Pane>
               <Tab.Pane eventKey="#link2">
                 <ListGroup>
-                  <ListGroup.Item>Autodesk 3ds Max</ListGroup.Item>
-                  <ListGroup.Item>Autodesk Premier Pro</ListGroup.Item>
-                  <ListGroup.Item>Design</ListGroup.Item>
-                  <ListGroup.Item>Animation</ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Autodesk 3ds Max
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Autodesk Premier Pro
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Design
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Animation
+                  </ListGroup.Item>
                 </ListGroup>
               </Tab.Pane>
               <Tab.Pane eventKey="#link3">
                 <ListGroup>
-                  <ListGroup.Item>Autodesk 3ds Max</ListGroup.Item>
-                  <ListGroup.Item>Unreal Engine</ListGroup.Item>
-                  <ListGroup.Item>Adobe Photoshop</ListGroup.Item>
-                  <ListGroup.Item>Design</ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Autodesk 3ds Max
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Unreal Engine
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Adobe Photoshop
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Design
+                  </ListGroup.Item>
                 </ListGroup>
               </Tab.Pane>
               <Tab.Pane eventKey="#link4">
                 <ListGroup>
-                  <ListGroup.Item>HTML</ListGroup.Item>
-                  <ListGroup.Item>CSS</ListGroup.Item>
-                  <ListGroup.Item>JavaScript</ListGroup.Item>
-                  <ListGroup.Item>Git/GitHub</ListGroup.Item>
-                  <ListGroup.Item>Terminal/Bash</ListGroup.Item>
-                  <ListGroup.Item>WCAG</ListGroup.Item>
-                  <ListGroup.Item>SEO</ListGroup.Item>
-                  <ListGroup.Item>Agile</ListGroup.Item>
-                  <ListGroup.Item>Scrum/Kanban</ListGroup.Item>
-                  <ListGroup.Item>Adobe Illustrator</ListGroup.Item>
-                  <ListGroup.Item>Adobe Photoshop</ListGroup.Item>
-                  <ListGroup.Item>Design</ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    HTML
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    CSS
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    JavaScript
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Git/GitHub
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Terminal/Bash
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    WCAG
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    SEO
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Agile
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Scrum/Kanban
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Adobe Illustrator
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Adobe Photoshop
+                  </ListGroup.Item>
+                  <ListGroup.Item className="animate__animated animate__lightSpeedInLeft home__link--link">
+                    Design
+                  </ListGroup.Item>
                 </ListGroup>
               </Tab.Pane>
             </Tab.Content>
           </Col>
         </Tab.Container>
       </Row>
-
-      {/*<Col sm={6}>
-          <Card>
-            <Card.Title>3D Design and Animation</Card.Title>
-            <Card.Title>3D Film Production</Card.Title>
-            <Card.Title>3D Game Design</Card.Title>
-            <Card.Title>Front-end Development</Card.Title>
-          </Card>
-        </Col>
-
-        <Col sm={6}>
-          <Card>
-            <Card.Body>
-              <Card.Text>Autodesk 3ds Max</Card.Text>
-              <Card.Text>Adobe Photoshop</Card.Text>
-              <Card.Text>Design</Card.Text>
-              <Card.Text>Animation</Card.Text>
-            </Card.Body>
-          </Card>
-
-          <Card>
-            <Card.Body>
-              <Card.Text>Autodesk 3ds Max</Card.Text>
-              <Card.Text>Design</Card.Text>
-              <Card.Text>Animation</Card.Text>
-            </Card.Body>
-          </Card>
-
-          <Card>
-            <Card.Body>
-              <Card.Text>Autodesk 3ds Max</Card.Text>
-              <Card.Text>Unreal Engine</Card.Text>
-              <Card.Text>Adobe Photoshop</Card.Text>
-              <Card.Text>Design</Card.Text>
-              <Card.Text>Animation</Card.Text>
-            </Card.Body>
-          </Card>
-
-          <Card>
-            <Card.Body>
-              <Card.Text>HTML</Card.Text>
-              <Card.Text>CSS</Card.Text>
-              <Card.Text>JavaScript</Card.Text>
-              <Card.Text>Git/GitHub</Card.Text>
-              <Card.Text>Terminal/Bash</Card.Text>
-              <Card.Text>WCAG</Card.Text>
-              <Card.Text>SEO</Card.Text>
-              <Card.Text>Agile</Card.Text>
-              <Card.Text>Scrum/Kanban</Card.Text>
-              <Card.Text>Adobe Illustrator</Card.Text>
-              <Card.Text>Adobe Photoshop</Card.Text>
-              <Card.Text>Design</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>*/}
     </section>
   );
 }
