0945f4770685d1fb0fd08ef873fa355e1383af41
ca2roaldsoy <roaldsoyen@gmail.com>
2020-06-29 22:37:39 +0200

    added projects

---
+++ src/constants/myProjects.js
@@ -0,0 +1,53 @@
+export const myProjects = [
+  {
+    title: "Ammibo",
+    script: "React",
+    style: "Sass",
+    code: "Html",
+    image:
+      "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5ed2d44f58c052310ac15d9b/screenshot.png",
+    github: "https://github.com/ca2roaldsoy/Portfolio-2_Ajax-and-Typescript",
+    web: "https://amiibo-characters.netlify.app/",
+  },
+  {
+    title: "Card Game",
+    script: "React",
+    style: "Sass",
+    code: "Html",
+    image:
+      "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5ed2dbea87e8c7076b3a898d/screenshot.png",
+    github: "https://github.com/ca2roaldsoy/Portfolio2-JS1",
+    web: "https://gameofcards.netlify.app/",
+  },
+  {
+    title: "January Island Escapes",
+    script: "Vue",
+    style: "Sass/CSS",
+    code: "Html",
+    image:
+      "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5efa4c008e8d5193bd18fc58/screenshot.png",
+    github: "https://github.com/ca2roaldsoy/Portfolio2-CSS-Frameworks",
+    web: "https://jolly-wing-c740e9.netlify.app/#/",
+  },
+  {
+    title: "Holidaze",
+    script: "React",
+    style: "Sass",
+    code: "Html",
+    image:
+      "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5efa4820fa7c3a01d1a57473/screenshot.png",
+    github: "https://github.com/ca2roaldsoy/ProjectExam-2",
+    web: "https://determined-sinoussi-faf075.netlify.app/",
+  },
+  {
+    title: "Game of Thrones Game",
+    subTitle: "Non-responsive board",
+    script: "JavaScript",
+    style: "Sass",
+    code: "Html",
+    image:
+      "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5ed2e1f902b1491744b588ed/screenshot.png",
+    github: "https://github.com/ca2roaldsoy/SemesterProject2019",
+    web: "https://reach-the-throne.netlify.app/",
+  },
+];
