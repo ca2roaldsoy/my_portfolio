d0a9e1f8a3e98a053c83ec011ad6a8950f4a4bfb
ca2roaldsoy <roaldsoyen@gmail.com>
2020-09-08 14:02:49 +0200

    changed web adress on projects

--- src/constants/myProjects.js
+++ src/constants/myProjects.js
@@ -7,7 +7,7 @@ export const myProjects = [
     image:
       "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5ed2d44f58c052310ac15d9b/screenshot.png",
     github: "https://github.com/ca2roaldsoy/Portfolio-2_Ajax-and-Typescript",
-    web: "https://amiibo-characters.netlify.app/",
+    web: "https://amiibo-characters.netlify.app/"
   },
   {
     title: "Card Game",
@@ -17,7 +17,7 @@ export const myProjects = [
     image:
       "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5ed2dbea87e8c7076b3a898d/screenshot.png",
     github: "https://github.com/ca2roaldsoy/Portfolio2-JS1",
-    web: "https://gameofcards.netlify.app/",
+    web: "https://gameofcards.netlify.app/"
   },
   {
     title: "January Island Escapes",
@@ -27,7 +27,7 @@ export const myProjects = [
     image:
       "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5efa4c008e8d5193bd18fc58/screenshot.png",
     github: "https://github.com/ca2roaldsoy/Portfolio2-CSS-Frameworks",
-    web: "https://jolly-wing-c740e9.netlify.app/#/",
+    web: "https://jolly-wing-c740e9.netlify.app/#/"
   },
   {
     title: "Holidaze",
@@ -35,9 +35,9 @@ export const myProjects = [
     style: "Sass",
     code: "Html",
     image:
-      "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5efa4820fa7c3a01d1a57473/screenshot.png",
+      "https://d33wubrfki0l68.cloudfront.net/5f5762e5be2aef00d559decc/screenshot.png",
     github: "https://github.com/ca2roaldsoy/ProjectExam-2",
-    web: "https://determined-sinoussi-faf075.netlify.app/",
+    web: "https://musing-hugle-fda8d4.netlify.app/"
   },
   {
     title: "Game of Thrones Game",
@@ -48,18 +48,6 @@ export const myProjects = [
     image:
       "https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5ed2e1f902b1491744b588ed/screenshot.png",
     github: "https://github.com/ca2roaldsoy/SemesterProject2019",
-    web:
-      "http://catonoroff.com/Noroff/Portfolio1/New%20Programming%20Foundation/Program%20Foundation%20CA/index_2.html",
-  },
-  {
-    title: "Sound & Music",
-    script: "JavaScript",
-    style: "CSS",
-    code: "Html",
-    image:
-      "https://cdn.pixabay.com/photo/2018/08/27/10/11/radio-cassette-3634616_960_720.png",
-    github: "https://github.com/ca2roaldsoy/ProgrammingFoundation.git",
-    web:
-      "http://catonoroff.com/Noroff/Portfolio1/New%20Programming%20Foundation/Program%20Foundation%20CA/index_2.html",
-  },
+    web: "https://xenodochial-ritchie-d0f767.netlify.app/index.html"
+  }
 ];
