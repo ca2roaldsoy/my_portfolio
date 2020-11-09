7f8d4b05244923fc48cbb0a9a445bf66369d2c6b
ca2roaldsoy <roaldsoyen@gmail.com>
2020-11-09 11:35:12 +0100

    changed subtitÃle for holidaze project

--- src/constants/myProjects.js
+++ src/constants/myProjects.js
@@ -31,7 +31,7 @@ export const myProjects = [
   },
   {
     title: "Holidaze",
-    subTitle: "the key to this project is no longer working, but you can always check my github for this project :)",
+    subTitle: "this project is sadly no longer working, but you can always check my github for this project :)",
     script: "React",
     style: "Sass",
     code: "Html",
